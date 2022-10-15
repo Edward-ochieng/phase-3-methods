f4d4f21c87e11acede6e74dcb946030b71ebb9a1
Edward <edward.ochieng@student.moringaschool.com>
2022-10-15 15:46:27 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
-# Your code here!
+def greet_programmer
+
+    puts "Hello, programmer!"
+    
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name= "Naureen")
+    puts "Hello, #{name}!"
+    greet_programmer
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+sum = add(2, 5)
+
+def halve( n = 6)
+    if n === 6
+        n/2
+
+    else
+        nil
+    end
+end
+
+    
+        
+    
+
+
+
+   
+
+    
+
+
